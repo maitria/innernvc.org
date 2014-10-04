526de79e18425214127c4581d6b4bed51a84f93b
Angela Harms <angela.harms@gmail.com>
2014-10-04 15:12:40 -0400

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo "==========================================="
+echo
+echo
+echo
+git status
+echo
+
+check_if_committed=`git status --porcelain`
+if ! [[ "$check_if_committed" = "" ]]
+then
+  echo "=========================================="
+  echo "No, you can't deploy without committing."
+  echo "=========================================="
+  exit 1
+fi
+
+branch=`git rev-parse --abbrev-ref HEAD`
+if ! [[ "$branch" = "master" ]]
+then
+  echo "=========================================="
+  echo "NO NO NO you can't deploy from this branch"
+  echo "=========================================="
+  exit 1
+fi
+
+echo 'pushing to master'
+git push origin master:master
+echo 'pushed'
+echo
+
+echo 'deploying to maitria webserver'
+rsync -r --progress --delete ./site/ root@104.131.105.47:/srv/innernvc.org 
+echo 'deployed'
